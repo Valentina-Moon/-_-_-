19aeb5658302bfa522fefa8328138291f918596f
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-05 19:58:14 +0300

    является ли первое число квадратом второго

---
+++ task№1/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = 2808;
+int numberB = 53;
+int numberC = numberB*numberB;
+if(numberA < numberC)
+  {
+                Console.WriteLine("первое число не является квадратом второго");
+            }
+else if (numberA > numberC)
+{
+    Console.WriteLine("первое число не является квадратом второго");
+}
+else
+{
+    Console.WriteLine("первое число является квадратом второго");
+}
