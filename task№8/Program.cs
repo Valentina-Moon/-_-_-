7e5cc2d6e7aa9233f6f002be5df6d458922f5ce8
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-06 12:03:58 +0300

    нибольшее значения числа из отрезка от 10 до 99

--- task№8/Program.cs
+++ task№8/Program.cs
@@ -1,5 +1,21 @@
-﻿string myString = "764";
-
-string newString = myString.Replace("6", string.Empty);
- 
-Console.WriteLine(newString);
+﻿int numberA = 48;
+int numberB = numberA/10;
+int numberC = numberA%10;
+if(numberB == numberC)
+{
+    Console.WriteLine(numberA);
+    Console.WriteLine(numberB);
+    Console.WriteLine("все наибольшие");
+}
+else if(numberB > numberC)
+{
+    Console.WriteLine(numberA);
+    Console.WriteLine(numberB);
+    Console.WriteLine("первое значение больше");
+}
+else
+{
+    Console.WriteLine(numberA);
+    Console.WriteLine(numberC);
+    Console.WriteLine("второе значенте больше");
+}
