28dc281687a215b2482733fa660a5b57b1340bbc
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-06 13:14:06 +0300

    кратно ли число, если нет вывести остаток

---
+++ task№10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+double numberA = 668;
+double numberB = 17;
+if(numberA%numberB  == 0)
+{
+    Console.WriteLine("нет остатка, число кратно");
+}
+else
+{
+    Console.Write("число не кратно, остаток:");
+    Console.WriteLine(numberA%numberB );
+}
